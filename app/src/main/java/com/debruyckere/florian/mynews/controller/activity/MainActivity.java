4a8017edbc9c1e0f397d4e2c7e71f8867f590cc2
Debruyckere <fldebruyck@numericable.fr>
2018-05-10 18:17:04 +0200

    save Skeleton

--- app/src/main/java/com/debruyckere/florian/mynews/controller/activity/MainActivity.java
+++ app/src/main/java/com/debruyckere/florian/mynews/controller/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.debruyckere.florian.mynews;
+package com.debruyckere.florian.mynews.controller.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.debruyckere.florian.mynews.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/debruyckere/florian/mynews/model/News.java
@@ -0,0 +1,7 @@
+package com.debruyckere.florian.mynews.model;
+
+/**
+ * Created by Debruyckère Florian on 10/05/2018.
+ */
+public class News {
+}
