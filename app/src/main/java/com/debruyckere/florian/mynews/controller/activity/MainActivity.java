724e14c970f6b5e4e3798234b73d7e752a78e9dc
Debruyckere <fldebruyck@numericable.fr>
2018-07-20 18:07:24 +0200

    add Notification

--- app/src/main/java/com/debruyckere/florian/mynews/controller/activity/MainActivity.java
+++ app/src/main/java/com/debruyckere/florian/mynews/controller/activity/MainActivity.java
@@ -1,7 +1,17 @@
 package com.debruyckere.florian.mynews.controller.activity;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SystemClock;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,7 +22,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.debruyckere.florian.mynews.R;
+import com.debruyckere.florian.mynews.model.News;
+import com.debruyckere.florian.mynews.model.NewsDownload;
 import com.debruyckere.florian.mynews.model.PageAdapter;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.BindView;
 
@@ -31,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
 
         configureViewPager();
+        createNotificationChannel();
+        configureAlarmManager();
     }
 
     private void configureViewPager(){
@@ -72,6 +90,86 @@ public class MainActivity extends AppCompatActivity {
             default:
                 Log.d("OPTION SELECT","Default item selected");
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /*------------
+      Notification
+      ------------*/
+
+    public void createNotificationChannel(){
+        CharSequence name = "MyNews Channel";
+        String description = "Channel use for Notification of MyNews app";
+        int notificationID = 0;
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel("MyNewsChannel",name,importance);
+        channel.setDescription(description);
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(notificationID,NotificationConfiguration().build());
+    }
+
+    public NotificationCompat.Builder NotificationConfiguration(){
+
+        return new NotificationCompat.Builder(this,"MyNewsChannel")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("MyNews new news")
+                .setContentText("A new news has arrived")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+    }
+    /*--------------
+      Alarm Manager
+      --------------*/
+
+    public void configureAlarmManager(){
+        AlarmManager alarm =(AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        PendingIntent pi = PendingIntent.getActivity(this,0,new Intent(this, AlarmReceiver.class),0);
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR,7);
+
+        SharedPreferences shared = getSharedPreferences("PARAMETER",MODE_PRIVATE);
+
+
+        if(shared.getBoolean("paramEnable",false)){
+
+            alarm.set(AlarmManager.ELAPSED_REALTIME, cal.getTimeInMillis(),pi);
+
+        }else{
+            alarm.cancel(pi);
+        }
+
+    }
+
+    public class AlarmReceiver extends BroadcastReceiver implements NewsDownload.Listeners{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            new NewsDownload(this,"https://api.nytimes.com/svc/topstories/v2/home.json?api-key=1ae7b601c1c7409796be77cce450f631",
+                    getBaseContext(),true)
+                    .execute();
+        }
+
+        @Override
+        public void onPreExecute() {
+
+        }
+
+        @Override
+        public void doInBackground() {
+
+        }
+
+        @Override
+        public void onPostExecute(ArrayList<News> news) {
+            if(news.size()== 0){
+                //launch notification
+                Log.i("ALARM","there new news");
+
+                NotificationManagerCompat notiManager = NotificationManagerCompat.from(getBaseContext());
+                notiManager.notify(0,NotificationConfiguration().build());
+            }
         }
     }
 }
