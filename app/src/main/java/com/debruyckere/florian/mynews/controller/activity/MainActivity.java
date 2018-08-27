9b59187bcea6bad633c745640c5d69b2e8dac613
Debruyckere <fldebruyck@numericable.fr>
2018-08-27 13:10:27 +0200

    fix circleci

--- app/src/main/java/com/debruyckere/florian/mynews/controller/activity/MainActivity.java
+++ app/src/main/java/com/debruyckere/florian/mynews/controller/activity/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         configureAlarmManager();
         configureDrawerLayout();
         configureNavigationView();
-        
+
     }
 
     private void configureViewPager(){
