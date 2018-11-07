1d4886ceab6849de850301ed5f19aa082f47ec53
Debruyckere <fldebruyck@numericable.fr>
2018-11-07 17:54:11 +0100

    add first instrument test

--- app/src/androidTest/java/com/debruyckere/florian/mynews/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/debruyckere/florian/mynews/ExampleInstrumentedTest.java
@@ -3,9 +3,15 @@ package com.debruyckere.florian.mynews;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+
+import com.debruyckere.florian.mynews.model.News;
+import com.debruyckere.florian.mynews.model.NewsDownload;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -17,10 +23,43 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    public void DownloadTest() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+        final ArrayList<News> toTest = new ArrayList<>();
+        final Object sync= new Object();
 
-        assertEquals("com.debruyckere.florian.mynews", appContext.getPackageName());
+        new NewsDownload(new NewsDownload.Listeners() {
+            @Override
+            public void onPreExecute() {
+                Log.i("TEST","Inside entrance");
+            }
+
+            @Override
+            public void doInBackground() {
+                Log.i("TEST","Inside deep");
+            }
+
+            @Override
+            public void onPostExecute(ArrayList<News> news) {
+                Log.i("TEST","Inside out");
+                toTest.addAll(news);
+                synchronized (sync){
+                    sync.notify();
+                }
+            }
+        },"https://api.nytimes.com/svc/topstories/v2/home.json?api-key=1ae7b601c1c7409796be77cce450f631"
+                ,appContext,false)
+                .execute();
+
+        synchronized (sync){
+            try {
+                sync.wait();
+            }catch (InterruptedException e){
+                Log.i("TEST","fail");
+            }
+
+        }
+        assertTrue(toTest.size()!=0);
     }
 }
